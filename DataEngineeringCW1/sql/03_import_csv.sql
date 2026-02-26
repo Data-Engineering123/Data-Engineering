91e836e13526c422093cc5e6c6dd1e0cf1fdaec6
Alvi Hossain <alvi997@outlook.com>
2026-02-26 16:34:56 +0000

    changes

--- DataEngineeringCW1/sql/03_import_csv.sql
+++ DataEngineeringCW1/sql/03_import_csv.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 -- Task 2: Import CSV into MySQL
 -- IMPORTANT: Adjust file path to your machine path.
 
@@ -10,3 +11,17 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (title, author, publication_year, price_gbp, rating, availability, product_url, scraped_at_utc);
+=======
+-- Task 2: Import CSV into MySQL
+-- IMPORTANT: Adjust file path to your machine path.
+
+USE techreads_db;
+
+LOAD DATA LOCAL INFILE 'c:/Users/alvi9/MyWork/Data Engineering - Copy/data/techreads_books.csv'
+INTO TABLE techreads_books
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(title, author, publication_year, price_gbp, rating, availability, product_url, scraped_at_utc);
+>>>>>>> 2a194767c6d33f5956eb227f2088e57a9185b9ab
--- DataEngineeringCW1/sql/04_task_query.sql
+++ DataEngineeringCW1/sql/04_task_query.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 -- Task 2 mandatory query:
 -- Extract three columns and sort by one column.
 
@@ -9,3 +10,16 @@ SELECT
     rating
 FROM techreads_books
 ORDER BY rating DESC, price_gbp ASC;
+=======
+-- Task 2 mandatory query:
+-- Extract three columns and sort by one column.
+
+USE techreads_db;
+
+SELECT
+    title,
+    price_gbp,
+    rating
+FROM techreads_books
+ORDER BY rating DESC, price_gbp ASC;
+>>>>>>> 2a194767c6d33f5956eb227f2088e57a9185b9ab
