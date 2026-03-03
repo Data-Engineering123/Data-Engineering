78f5a317b1c6455b98367a883ee3dcf0da0e11cc
Alvi Hossain <alvi997@outlook.com>
2026-03-03 16:47:31 +0000

    commited

--- sql/02_create_table.sql
+++ sql/02_create_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS techreads_books (
     author VARCHAR(255) NULL,
     publication_year INT NULL,
     price_gbp DECIMAL(10,2) NOT NULL,
-    rating TINYINT NOT NULL,
+    rating DECIMAL(3,1) NULL,
     availability VARCHAR(120) NULL,
     product_url VARCHAR(500) NULL,
     scraped_at_utc VARCHAR(60) NULL
