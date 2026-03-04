7bcf59ca08e4a022a85c0d18715e9026b33f6a0d
Nafisa Maliat <maliatn1@gmail.com>
2026-03-04 19:40:50 +0000

    Changes in SQL

--- sql/03_import_csv.sql
+++ sql/03_import_csv.sql
@@ -1,9 +1,8 @@
 -- Task 2: Import CSV into MySQL
--- IMPORTANT: Adjust file path to your machine path.
 
 USE techreads_db;
 
-LOAD DATA LOCAL INFILE 'c:/Users/alvi9/MyWork/Data Engineering - Copy/data/techreads_books.csv'
+LOAD DATA LOCAL INFILE '/Users/nafisamaliat/Library/CloudStorage/OneDrive-UniversityofRoehampton/ROE/Year 3/Data Engineering/Data Engineering - Copy/data/techreads_books.csv'
 INTO TABLE techreads_books
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
